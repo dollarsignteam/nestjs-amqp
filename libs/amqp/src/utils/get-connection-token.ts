c8eed465cca341e17d7a2428d7d21ea5aed71271
chagridsada <chagridsada@dollarsign.co>
2021-07-02 16:41:55 +0700

    refactor(utils): update `getConnectionToken`

--- libs/amqp/src/utils/get-connection-token.ts
+++ libs/amqp/src/utils/get-connection-token.ts
@@ -1,10 +1,8 @@
 import { DEFAULT_CONNECTION_NAME } from '../constants';
 import { AMQPModuleOptions } from '../interfaces';
 
-const defaultConnection = `${DEFAULT_CONNECTION_NAME}_CONNECTION`;
-
 function getConnectionName(name: string): string {
-  return name ? `${name}_CONNECTION`.toUpperCase() : defaultConnection;
+  return name ? `${name}:connection`.toLowerCase().replace(/\s+/g, '-') : DEFAULT_CONNECTION_NAME;
 }
 
 export function getConnectionToken(connection?: AMQPModuleOptions | string): string {
--- libs/amqp/src/utils/spec/get-connection-token.spec.ts
+++ libs/amqp/src/utils/spec/get-connection-token.spec.ts
@@ -1,30 +1,30 @@
 import { AMQPModuleOptions } from '../../interfaces';
 import { getConnectionToken } from '../get-connection-token';
 describe('getConnectionToken', () => {
-  it('should return `DEFAULT_CONNECTION`', () => {
+  it('should return `default:connection`', () => {
     const result = getConnectionToken();
-    expect(result).toBe('DEFAULT_CONNECTION');
+    expect(result).toBe('default:connection');
   });
 
-  it('should return `DEFAULT_CONNECTION` when input is empty string', () => {
+  it('should return `default:connection` when input is empty string', () => {
     const result = getConnectionToken('');
-    expect(result).toBe('DEFAULT_CONNECTION');
+    expect(result).toBe('default:connection');
   });
 
-  it('should return `TEST_CONNECTION` when input is `test`', () => {
+  it('should return `test:connection` when input is `test`', () => {
     const result = getConnectionToken('test');
-    expect(result).toBe('TEST_CONNECTION');
+    expect(result).toBe('test:connection');
   });
 
-  it('should return `DEFAULT_CONNECTION` when input is empty option', () => {
+  it('should return `default:connection` when input is empty option', () => {
     const option: AMQPModuleOptions = {};
     const result = getConnectionToken(option);
-    expect(result).toBe('DEFAULT_CONNECTION');
+    expect(result).toBe('default:connection');
   });
 
-  it('should return `DEMO_CONNECTION` when option name is `demo`', () => {
+  it('should return `demo:connection` when option name is `demo`', () => {
     const option: AMQPModuleOptions = { name: 'demo' };
     const result = getConnectionToken(option);
-    expect(result).toBe('DEMO_CONNECTION');
+    expect(result).toBe('demo:connection');
   });
 });
