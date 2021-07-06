4389175d5a78001f8ea040f1cc072f7ffe8c77cf
chagridsada <chagridsada@dollarsign.co>
2021-07-07 02:11:03 +0700

    fix(app): update demo example

--- src/app.service.ts
+++ src/app.service.ts
@@ -14,39 +14,37 @@ export class AppService {
   }
 
   async sendMessage1(): Promise<string> {
-    const name = 'default';
     const options: SendOptions = {
       message_id: new Date().getTime(),
     };
-    const message = { name };
+    const message = { now: new Date().toISOString() };
     const result = await this.producer.send('demo1', message, options);
-    return `Send message to ${name} connection: ${result}`;
+    return `Send message to demo1 of default connection: ${result}`;
   }
 
   async sendMessage2(): Promise<string> {
-    const name = 'amqp';
     const options: SendOptions = {
       message_id: new Date().getTime(),
-      connectionName: name,
+      connectionName: 'custom',
       group_id: 'A',
     };
-    const message = { name };
+    const message = { now: new Date().toISOString() };
     const result = await this.producer.send('demo2', message, options);
-    return `Send message to ${name} connection: ${result}`;
+    return `Send message to demo2 of custom connection: ${result}`;
   }
 
   @Consumer('demo1', { parallelMessageProcessing: 1, concurrency: 2 })
   async receiveMessage1(data: unknown, control: MessageControl): Promise<void> {
     const { message_id } = control.message;
-    this.logger.trace(`Received message from amqp1 id: ${message_id}`, data);
-    await delay(3000);
+    this.logger.trace(`Received message from demo1 id: ${message_id}`, data);
+    await delay(2000);
   }
 
-  @Consumer('demo2', { connectionName: 'amqp', parallelMessageProcessing: 1, concurrency: 2 })
+  @Consumer('demo2', { connectionName: 'custom', concurrency: 2 })
   async receiveMessage2(data: unknown, control: MessageControl): Promise<void> {
     const { message_id } = control.message;
-    this.logger.trace(`Received message from amqp2 id: ${message_id}`, data);
-    await delay(3000);
+    this.logger.trace(`Received message from demo2 id: ${message_id}`, data);
+    await delay(2000);
     control.accept();
   }
 }
