f98d94e455a12e007b6d4e5370be17967d6e7e8e
chagridsada <chagridsada@dollarsign.co>
2021-07-12 00:48:56 +0700

    fix(utils): change `getErrorMessage` to `ErrorMessage`

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,6 +1,6 @@
+export * from './error-message';
 export * from './get-connection-token';
 export * from './get-consumer-token';
-export * from './get-error-message';
 export * from './get-id';
 export * from './get-logger';
 export * from './get-producer-token';
